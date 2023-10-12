01bd1fab4176625d2f0e9606183b9356893fae43
Mihir Pesswani <pesswani.mihir@gmail.com>
2023-10-12 15:04:26 +0530

    added audio recording stream functionality

--- src/components/FileDisplay.jsx
+++ src/components/FileDisplay.jsx
@@ -4,13 +4,13 @@ function FileDisplay(props) {
   const { handleAudioReset, file, audioStream } = props;
 
   return (
-    <main className="flex-1 flex flex-col p-4 gap-3 text-center sm:gap-4 justify-center pb-20 min-w-72 w-72 sm:w-96 max-w-full mx-auto">
+    <main className="flex-1 p-4 flex flex-col gap-3 text-center sm:gap-4 justify-center pb-20 w-72 sm:w-96 max-w-full mx-auto">
       <h1 className="font-semibold text-4xl sm:text-5xl md:text-6xl">
         Your <span className="text-blue-400 bold font-semibold">File</span>
       </h1>
       <div className="flex flex-col text-left my-4">
         <h3 className="font-semibold">Name</h3>
-        <p>{file.name}</p>
+        <p>{file ? file?.name : "Custom Audio"}</p>
       </div>
       <div className="flex items-center justify-between gap-4">
         <button
