f4125a804935cbaabdf98ec7cdccec08e342c1c5
Mihir Pesswani <pesswani.mihir@gmail.com>
2023-10-12 14:22:24 +0530

    added audio file acceptance changes

--- src/components/FileDisplay.jsx
+++ src/components/FileDisplay.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 
-function FileDisplay() {
+function FileDisplay(props) {
+  const { file, audioStream } = props;
+
   return (
     <main className="flex-1 flex flex-col p-4 gap-3 text-center sm:gap-4 justify-center pb-20 min-w-72 w-72 sm:w-96 max-w-full mx-auto">
       <h1 className="font-semibold text-4xl sm:text-5xl md:text-6xl">
@@ -8,7 +10,7 @@ function FileDisplay() {
       </h1>
       <div className="flex flex-col text-left my-4">
         <h3 className="font-semibold">Name</h3>
-        <p>file.name</p>
+        <p>{file.name}</p>
       </div>
       <div className="flex items-center justify-between gap-4">
         <button className="text-slate-400 hover:text-blue-600 duration-200">
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 
-function HomePage() {
+function HomePage(props) {
+  const { setFile, setAudioStream } = props;
+
   return (
     <main className="flex-1 p-4 flex flex-col gap-3 text-center sm:gap-4 justify-center pb-20">
       <h1 className="font-semibold text-5xl sm:text-6xl md:text-7xl">
@@ -18,7 +20,15 @@ function HomePage() {
         Or{" "}
         <label className="text-blue-400 cursor-pointer hover:text-blue-600 duration-200">
           upload{" "}
-          <input className="hidden" type="file" accept=".mp3, .wav"></input>
+          <input
+            onChange={(e) => {
+              const tempFile = e.target.files[0];
+              setFile(tempFile);
+            }}
+            className="hidden"
+            type="file"
+            accept=".mp3, .wav"
+          ></input>
         </label>
         a mp3 file
       </p>
