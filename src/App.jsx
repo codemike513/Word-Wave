bef3dcc70ee3d1cf8478bf5dccf37c63033281cd
Mihir Pesswani <pesswani.mihir@gmail.com>
2023-10-12 14:08:56 +0530

    added filedisplay component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Header from "./components/Header";
 import HomePage from "./components/HomePage";
+import FileDisplay from "./components/FileDisplay";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -10,6 +11,7 @@ function App() {
       <section className="min-h-screen flex flex-col">
         <Header />
         <HomePage />
+        <FileDisplay />
       </section>
     </div>
   );
