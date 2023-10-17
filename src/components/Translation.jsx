3d8233bb9c40c71f9efa8613d4c778d95cbb16bd
Mihir Pesswani <pesswani.mihir@gmail.com>
2023-10-17 22:49:39 +0530

    added the translation component

---
+++ src/components/Translation.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { LANGUAGES } from "../utils/presets";
+
+function Translation(props) {
+  const {
+    textElement,
+    toLanguage,
+    translating,
+    setToLanguage,
+    generateTranslation,
+  } = props;
+
+  return (
+    <>
+      {/* {textElement && !translating && <p>{textElement}</p>} */}
+      <div className="flex flex-col gap-2 mx-w-[400px] w-full mx-auto">
+        {!translating && (
+          <div className="flex flex-col">
+            <p className="text-xs sm:text-sm font-medium text-slate-500 mr-auto">
+              To language
+            </p>
+            <div className="flex items-stretch gap-2">
+              <select
+                className="flex-1 outline-none bg-white focus:outline-none border border-solid border-transparent hover:border-blue-300 duration-200 p-2 rounded"
+                value={toLanguage}
+                onChange={(e) => setToLanguage(e.target.value)}
+              >
+                <option value={"Select Language"}>Select Language</option>
+                {Object.entries(LANGUAGES).map(([key, value]) => {
+                  return (
+                    <option key={key} value={value}>
+                      {key}
+                    </option>
+                  );
+                })}
+              </select>
+              <button
+                onClick={generateTranslation}
+                className="specialBtn px-3 py-2 rounded-lg text-blue-400 hover:text-blue-600 duration-200"
+              >
+                Translate
+              </button>
+            </div>
+          </div>
+        )}
+        {textElement && !translating && <p>{textElement}</p>}
+        {/* {translating && (
+          <div className="grid place-items-center">
+            <i className="fa-solid fa-spinner animate-spin"></i>
+          </div>
+        )} */}
+      </div>
+    </>
+  );
+}
+
+export default Translation;
